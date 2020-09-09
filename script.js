3602356cc4d053431804f241e96f4b33513683da
somebody1503 <64851154+somebody1503@users.noreply.github.com>
2020-09-09 14:21:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.onload = function() {
     console.log('Starting Daeun World');
 };
+
+
+onclick()
+function button1_click(){
+    var comment = prompt("발명록을 작성해주세요!");
+    document.write(comment);
+}
